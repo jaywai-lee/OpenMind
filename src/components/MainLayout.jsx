e16036d5ef892256f297ef4a51f27f1a1fe463e6
unknown <jylee2670@gmail.com>
2025-12-16 20:38:41 +0900

    ✨ Feat: test33

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,4 +1,11 @@
 function MainLayout() {
-  return <>111test123테스트성공-수정했어요!</>;
+  return (
+    <div>
+      <h1>111test123테스트성공-수정했어요!</h1>
+      <h2>테스트테스트</h2>
+      <h2>테스트입니다</h2>
+      <h2>mergeConflictTest</h2>
+    </div>
+  );
 }
 export default MainLayout;

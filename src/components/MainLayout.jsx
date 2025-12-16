ce293f94d2ebdb5e2dc55aa3fc38d402d1937d0f
mukiiru <mukiiru@gmail.com>
2025-12-16 20:57:33 +0900

    git 모의  테스트

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -5,20 +5,22 @@ function MainLayout() {
 
   const aaa = () => {
     console.log('aaaaaa');
-    console.log('nnnnnn');
+    console.log('cccc');
+    console.log('tessssst');
     console.log('cccccc');
     console.log('dddddd');
-    console.log('wwwwww');
+    console.log('dddddd');
     console.log('cccccc');
   };
 
   return (
     <>
-      <div>111test123테스트성공-수정했어요!</div>
-      <div>111test123테스트성공-수정했어요!</div>
-      <div>111test123테스트성공-수정했어요!</div>
-      <div>111test123테스트성공-수정했어요!</div>
-      <div>111test123테스트성공-수정했어요!</div>
+      <div>111test123테스트성공-aaaa</div>
+      <div>111test123테스트성공-vvvvvv</div>
+      <div>테스트중</div>
+      <div>111test123테스트성공-aaaaa</div>
+      <div>111test123테스트성공-bbb</div>
+      <div>111test123테스트성공-ccccc</div>
     </>
   );
 }

98cfb1992ffde9f72e3b0e573c104d9297a84f41
jihyoyoo <jihyo789@naver.com>
2025-12-16 20:38:38 +0900

    test

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -1,4 +1,25 @@
+import { useState } from 'react';
+
 function MainLayout() {
-  return <>111test123테스트성공-수정했어요!</>;
+  const [item, setItem] = useState;
+
+  const aaa = () => {
+    console.log('aaaaaa');
+    console.log('nnnnnn');
+    console.log('test');
+    console.log('dddddd');
+    console.log('wwwwww');
+    console.log('cccccc');
+  };
+
+  return (
+    <>
+      <div>111test123테스트성공-수정했어요!</div>
+      <div>111test123테스트성공-수정했어요!</div>
+      <div>테스트중</div>
+      <div>111test123테스트성공-수정했어요!</div>
+      <div>111test123테스트성공-수정했어요!</div>
+    </>
+  );
 }
 export default MainLayout;

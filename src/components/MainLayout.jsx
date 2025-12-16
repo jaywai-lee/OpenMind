b9677d18cbdf9eb59e5c03658dd95915367f692b
mukiiru <17698341+mukiiru@users.noreply.github.com>
2025-12-16 20:59:32 +0900

    Feature/aaaaa (#8)
    * git 모의  테스트
    
    * git 모의  테스트

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -5,6 +5,11 @@ function MainLayout() {
 
   const aaa = () => {
     console.log('aaaaaa');
+    console.log('cccc');
+    console.log('tessssst');
+    console.log('cccccc');
+    console.log('dddddd');
+    console.log('dddddd');
     console.log('nnnnnn');
     console.log('test');
     console.log('cccccc');
@@ -15,6 +20,12 @@ function MainLayout() {
 
   return (
     <>
+      <div>111test123테스트성공-aaaa</div>
+      <div>111test123테스트성공-vvvvvv</div>
+      <div>테스트중</div>
+      <div>111test123테스트성공-aaaaa</div>
+      <div>111test123테스트성공-bbb</div>
+      <div>111test123테스트성공-ccccc</div>
       <div>111test123테스트성공-수정했어요!</div>
       <div>111test123테스트성공-수정했어요!</div>
       <div>테스트중</div>

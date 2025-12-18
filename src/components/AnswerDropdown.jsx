214456e7275119e66db7580c3cb0e86a48bd9145
jihyoyoo <131952937+jihyoyoo@users.noreply.github.com>
2025-12-18 17:28:29 +0900

    ✨Feat: 답변하기 드롭다운 컴포넌트 추가 (#20)
    * ✨ Feat: 답변하기 피드카드 제작
    
    * 🐛 fix: 누락된 컴포넌트 요소 추가
    
    * ✨ Feat: 답변하기 드롭다운 컴포넌트 추가

---
+++ src/components/AnswerDropdown.jsx
@@ -0,0 +1,19 @@
+import styles from './AnswerDropdown.module.css';
+import close from '../assets/icons/close.svg';
+import edit from '../assets/icons/edit.svg';
+
+function AnswerDropdown() {
+  return (
+    <div className={styles.dropdownContainer}>
+      <div className={styles.dropdownGroup}>
+        <img className={styles.dropdownImage} src={edit} alt="" />
+        <span className={styles.dropdownText}>수정하기</span>
+      </div>
+      <div className={styles.dropdownGroup}>
+        <img className={styles.dropdownImage} src={close} alt="" />
+        <span className={styles.dropdownText}>삭제하기</span>
+      </div>
+    </div>
+  );
+}
+export default AnswerDropdown;

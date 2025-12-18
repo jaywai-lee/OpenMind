9c5aff6bba9c775afc8beaa885e976b3553017d9
jihyoyoo <jihyo789@naver.com>
2025-12-18 17:18:30 +0900

    ✨ Feat: 답변하기 드롭다운 컴포넌트 추가

--- src/components/FeedCardList.jsx
+++ src/components/FeedCardList.jsx
@@ -1,15 +1,26 @@
+import { useState } from 'react';
 import styles from './FeedCardList.module.css';
 import more from '../assets/icons/more.svg';
 import thumbsDown from '../assets/icons/thumbs-down.svg';
 import thumbsUp from '../assets/icons/thumbs-up.svg';
 import profileImage from '../assets/images/userimage-sample.png';
+import AnswerDropdown from './AnswerDropdown';
 
 function FeedCardList() {
+  const [ isDropdownOpen, setIsDropdownOpen ] = useState(false);
+
+  const handleClickMore = () => {
+    setIsDropdownOpen((prev) => !prev);
+  };
+
   return (
     <section className={styles.feedCardListContainer}>
       <div className={styles.answerActions}>
         <div className={styles.answerStatus}>미답변</div>
-        <img src={more} alt="" />
+        <div className={styles.dropdownGroup}>
+          <img className={styles.moreImage} onClick={handleClickMore} src={more} alt="" />
+          {isDropdownOpen &&  <AnswerDropdown />}
+        </div>
       </div>
       <div className={styles.question}>
         <span className={styles.questionMeta}>질문 · 2주전</span>
@@ -24,13 +35,13 @@ function FeedCardList() {
         </div>
       </div>
       <div className={styles.reactions}>
-        <div className={styles.reactionGroup}>
+        <div className={styles.reactionGroup}> 
           <div className={styles.reactionButton}>
-            <img src={thumbsUp} alt="" />
+            <img className={styles.reactionImg} src={thumbsUp} alt="" />
             <span>좋아요</span>
           </div>
           <div className={styles.reactionButton}>
-            <img src={thumbsDown} alt="" />
+            <img className={styles.reactionImg} src={thumbsDown} alt="" />
             <span>싫어요</span>
           </div>
         </div>

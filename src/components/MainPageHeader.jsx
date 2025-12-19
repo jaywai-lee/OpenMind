2fe806db8881bd1838a827de2108218f9af992b6
mukiiru <mukiiru@gmail.com>
2025-12-19 18:33:49 +0900

    ✨ 메인페이지 리스트페이지 링크 추가

--- src/components/MainPageHeader.jsx
+++ src/components/MainPageHeader.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import Button from './common/Button/Button';
 import styles from './MainPageHeader.module.css';
 function MainPageHeader() {
   return (
     <div className={styles.questionGoBtnContainer}>
       <header className={`${styles.questionGoBtnGroup}`}>
-        <Button theme="white">질문하러 가기</Button>
+        <Link to="/list">
+          <Button theme="white">질문하러 가기</Button>
+        </Link>
       </header>
     </div>
   );

3170e94a2b83f3733c50792c874f648d6423cf31
leehi9558 <leehi9558@gmail.com>
2025-12-22 18:11:16 +0900

    ✨ [ECS-87] 질문 리스트 페이지 헤더 답변하러가기 버튼링크

--- src/components/ListHeader.jsx
+++ src/components/ListHeader.jsx
@@ -2,14 +2,16 @@ import styles from './ListHeader.module.css';
 import logo from '../assets/icons/logo.svg';
 import { Link } from 'react-router-dom';
 import Button from './common/Button/Button';
+import storage from '../utils/storage';
 
 function ListHeader() {
+  const id = storage.get('userId');
   return (
     <div className={styles.group}>
       <a href="/">
         <img className={styles.logo} src={logo} alt="logo" />
       </a>
-      <Link to="/answer">
+      <Link to={`/post/${id}/answer`}>
         <Button theme="white">답변하러 가기</Button>
       </Link>
     </div>

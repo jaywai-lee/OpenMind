580f678604c7ec2dfdddc95d91fdea89f23b6eeb
codeit-이정용 <jylee2670@gmail.com>
2025-12-22 17:10:03 +0900

    ✨ [ECS-83] feat: 질문 생성 모달 연결 및 api 연동 (#46)

--- src/api/subjects.js
+++ src/api/subjects.js
@@ -6,5 +6,5 @@ export const createSubject = (name) =>
 export const getSubjectList = (params) =>
   instance.get(`/subjects/`, { params }).then((res) => res.data);
 
-export const getSubjects = (subjectId) =>
+export const getSubject = (subjectId) =>
   instance.get(`/subjects/${subjectId}/`).then((res) => res.data);
--- src/components/FeedHeader.jsx
+++ src/components/FeedHeader.jsx
@@ -2,6 +2,7 @@ import styles from './FeedHeader.module.css';
 import logo from '../assets/icons/logo.svg';
 import headerImage from '../assets/images/header-image.png';
 import UserProfile from './UserProfile';
+import { Link } from 'react-router-dom';
 
 function FeedHeader({ subject }) {
   return (
@@ -10,7 +11,9 @@ function FeedHeader({ subject }) {
         className={styles.header}
         style={{ backgroundImage: `url(${headerImage})` }}
       >
-        <img className={styles.logo} src={logo} alt="logo" />
+        <Link to="/" aria-label="메인 페이지">
+          <img className={styles.logo} src={logo} alt="logo" />
+        </Link>
       </header>
       {subject && <UserProfile subject={subject} />}
     </div>

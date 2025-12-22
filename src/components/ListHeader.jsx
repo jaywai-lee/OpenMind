a97fbfecc4269d6047f5fb5a67441d96192b408e
leehi9558 <leehi9558@gmail.com>
2025-12-22 20:08:29 +0900

    ✨ [ECS-44] 질문 페이지 헤더 답변하러가기 버튼 링크수정 (#51)
    * ✨ Feat: 질문 페이지 헤더 제작
    
    * ✨ Feat: [ECS-40] 질문 리스트 페이지 헤더 버튼스타일수정 및 링크추가
    
    * ✨ [ECS-87] 질문 리스트 페이지 헤더 답변하러가기 버튼링크
    
    * ✨ [ECS-44] 드롭다운구현중
    
    * ✨ [ECS-44] 질문 페이지 헤더 답변하러가기 버튼 링크수정

--- src/components/ListHeader.jsx
+++ src/components/ListHeader.jsx
@@ -1,19 +1,30 @@
 import styles from './ListHeader.module.css';
 import logo from '../assets/icons/logo.svg';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Button from './common/Button/Button';
 import storage from '../utils/storage';
 
 function ListHeader() {
+  const navigate = useNavigate();
   const id = storage.get('userId');
+  const handleLink = () => {
+    if (!id) {
+      navigate(`/`);
+    } else {
+      navigate(`/post/${id}/answer`);
+    }
+  };
   return (
     <div className={styles.group}>
       <a href="/">
         <img className={styles.logo} src={logo} alt="logo" />
       </a>
-      <Link to={`/post/${id}/answer`}>
-        <Button theme="white">답변하러 가기</Button>
-      </Link>
+
+      <div className={styles.linkButton}>
+        <Button theme="white" onClick={handleLink}>
+          답변하러 가기
+        </Button>
+      </div>
     </div>
   );
 }

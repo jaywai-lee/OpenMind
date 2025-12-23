f886b0c5fcb65705336f9b0063f61f356da53333
unknown <jylee2670@gmail.com>
2025-12-23 20:38:01 +0900

    ✨ [ECS-107] feat: 자신의 피드에서는 질문 불가 기능 추가

--- src/components/SubjectsFeedFooter.jsx
+++ src/components/SubjectsFeedFooter.jsx
@@ -1,10 +1,17 @@
 import styles from './SubjectsFeedFooter.module.css';
 import FloatingButton from './common/Button/FloatingButton';
 
-function SubjectsFeedFooter({ onOpenModal }) {
+function SubjectsFeedFooter({ isMyFeed, onOpenModal }) {
+  const handleClick = () => {
+    if (isMyFeed) {
+      alert('내 피드에는 질문을 작성할 수 없습니다.');
+      return;
+    }
+    onOpenModal();
+  };
   return (
     <div className={styles.floatingGroup}>
-      <FloatingButton onClick={onOpenModal}>
+      <FloatingButton onClick={handleClick}>
         <span>질문 작성</span>
         <span className={styles.desktopOnly}>하기</span>
       </FloatingButton>

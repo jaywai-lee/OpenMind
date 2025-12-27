e581e2c6d5d4d12c7a18c07d16e821c21ed1c65d
leehi9558 <leehi9558@gmail.com>
2025-12-27 12:46:42 +0900

    ♻️ [ECS-42-1] 질문 페이지 타이틀 코드수정

--- src/components/ListTitle.jsx
+++ src/components/ListTitle.jsx
@@ -1,7 +1,7 @@
 import styles from './ListTitle.module.css';
 
 function ListTitle() {
-  return <h1 className={styles.title}>누구에게 질문 할까요?</h1>;
+  return <h1 className={styles.title}>누구에게 질문할까요?</h1>;
 }
 
 export default ListTitle;

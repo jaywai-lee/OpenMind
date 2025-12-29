2fb7e21cf2cea93b2210acc3fade15f54bc82272
leehi9558 <leehi9558@gmail.com>
2025-12-29 17:47:49 +0900

    ✨ [ECS-122-1] 질문 리스트 카드리스트 반응형 (#104)

--- src/components/ListTitleDropDown.jsx
+++ src/components/ListTitleDropDown.jsx
@@ -1,55 +1,13 @@
 import { useState } from 'react';
 import styles from './ListTitleDropDown.module.css';
-import ArrowDown from '../assets/icons/arrow-down.svg?react';
+import DropDownMenu from './common/Dropdown/DropDownMenu';
 
 function ListTitleDropDown({ onSelect }) {
-  const [selectedOption, setSelectedOption] = useState('최신순');
-  const [isShowOptions, setIsShowOptions] = useState(false);
-
-  const options = [
-    { label: '이름순', value: 'name' },
-    { label: '최신순', value: 'createdAt' },
-  ];
-
-  const handleToggleOptions = () => setIsShowOptions((prev) => !prev);
-
-  const handleSelectOption = (option) => {
-    setSelectedOption(option.label);
-    setIsShowOptions(false);
-    onSelect?.(option.value);
-  };
-
   return (
-    <div className={styles.grop}>
+    <div className={styles.group}>
       <h1 className={styles.title}>누구에게 질문할까요?</h1>
 
-      <div className={styles.selectBox}>
-        <button
-          type="button"
-          onClick={handleToggleOptions}
-          className={`${styles.label} ${isShowOptions ? styles.active : ''}`}
-        >
-          {selectedOption}
-          {/* 회전 애니메이션을 위해 className 추가 */}
-          <ArrowDown
-            className={`${styles.arrow} ${isShowOptions ? styles.rotated : ''}`}
-          />
-        </button>
-
-        {isShowOptions && (
-          <ul className={styles.optionsList}>
-            {options.map((option) => (
-              <li
-                key={option.value}
-                onClick={() => handleSelectOption(option)}
-                className={styles.optionItem}
-              >
-                {option.label}
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
+      <DropDownMenu onChangeSort={onSelect} />
     </div>
   );
 }
